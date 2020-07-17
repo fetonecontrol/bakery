d1823ff656e5b4ac70a24b040ed9d0faf21e18e8
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-17 12:05:47 -0700

    add working ui that colects and stores user data

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,24 +7,22 @@ namespace Bakery.Models
   public class Bakery
   {
     public static void Main()
-    {
+    { 
       Console.WriteLine("Would you like to Purchase Bread, Pastry, or both?");
       Console.WriteLine("If you would like to order Bread, please enter a number,");
-      Console.WriteLine("Otherwise, enter any not numerical character to advance to the Pastry menu");
-      string breadOrderNumber = Console.ReadLine();
-      if (breadOrderNumber == @"\d+\")
-      {
-      int number = Int32.Parse(breadOrderNumber);
-      Bread loaves = new Bread(number);
+      Console.WriteLine("Otherwise, enter N to advance to the Pastry Menu");
+      string breadOrder = Console.ReadLine();
+      int breadOrderNumber = Int32.Parse(breadOrder);
+      Bread loaves = new Bread(breadOrderNumber);
       Console.WriteLine("The Price for Bread is: " + " " + loaves.NumberOfLoaves);
-      }
-      else 
-      {
+    
       Console.WriteLine("If you would like to order pastrys, please enter a number,");
       Console.WriteLine("Otherwise, enter N");
-      string loaves = Console.ReadLine();
+      string pastryOrder = Console.ReadLine();
+      int pastryOrderNumber = Int32.Parse(pastryOrder);
+      Pastry pastries = new Pastry(pastryOrderNumber);
+      Console.WriteLine("The Price for Pastries is: " + " " + pastries.NumberOfPastries);
       }
-      
+
     }
-  }
 }
