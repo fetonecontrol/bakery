064e81d9ed6c47d43533ae228ee49cee6ab46e90
Frederick Ernest <frederick.ernest@gmail.com>
2020-07-17 16:36:04 -0700

    clean up and refactor UI to more accurately represent program capabilities, and limitations

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,15 +10,14 @@ namespace Bakery.Models
     { 
       try
       {  
-        Console.WriteLine("If you would like to order Bread, please enter the number of Loaves you'd like.");
+        Console.WriteLine("Please enter the number of Loaves you'd like.");
         Console.WriteLine("Bread is 5 Ruppees a loaf and buy 2, get 1 free!");
         string breadOrder = Console.ReadLine();
         int breadOrderNumber = Int32.Parse(breadOrder);
         Bread loaves = new Bread(breadOrderNumber);
       
-        Console.WriteLine("If you would like to order pastrys, please enter the number of Pastries you'd like.");
+        Console.WriteLine("Please enter the number of Pastries you'd like.");
         Console.WriteLine("Pastries are 2 Ruppees each, or 3 for 5!");
-        Console.WriteLine("Otherwise, enter N");
         string pastryOrder = Console.ReadLine();
         int pastryOrderNumber = Int32.Parse(pastryOrder);
         Pastry pastries = new Pastry(pastryOrderNumber);
@@ -26,20 +25,16 @@ namespace Bakery.Models
         Console.WriteLine("The Price for Pastries is: " + " " + pastries.PastryPrice() + " " + "Ruppees");
         Order total = new Order(loaves.BreadPrice(), pastries.PastryPrice());
         Console.WriteLine("The Total Price of Your Order is: " + " " + total.OrderTotal + " " + "Ruppees");
-        Console.WriteLine("Would You Like to Place Another Order? Enter Y or N");
+        Console.WriteLine("If you would like to place another order, enter Y, all other input will end program.");
         string cont = Console.ReadLine();
         if (cont == "Y")
         {
           Console.WriteLine("Here We Go Again!");
           Main();
         }
-        else if (cont == "N")
+        else
         {
           Console.WriteLine("Thanks for Stopping By! There's Monsters Out There, Travel Safe!");
-        }
-        else if (cont != "Y" && cont != "N")
-        {
-          Console.WriteLine("Invalid Selection, Please Choose Y or N -CaSe-SeNSaTIVe- ");
         }
       }
       catch (Exception e)
